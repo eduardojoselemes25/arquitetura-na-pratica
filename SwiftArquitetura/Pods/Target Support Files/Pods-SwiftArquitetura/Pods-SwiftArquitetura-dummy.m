19bf197c023148bf55cf0fe09a89ae2679a5d13e
Eduardo Jose Lemes <eduardojoselemes25@gmail.com>
2024-01-29 16:50:27 -0300

    pasta api

---
+++ SwiftArquitetura/Pods/Target Support Files/Pods-SwiftArquitetura/Pods-SwiftArquitetura-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SwiftArquitetura : NSObject
+@end
+@implementation PodsDummy_Pods_SwiftArquitetura
+@end
---
+++ SwiftArquitetura/SwiftArquitetura/Source/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftArquitetura
+//
+//  Created by Eduardo lemes on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
